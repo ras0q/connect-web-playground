3015e58998b123642c2881a7cae8bf3d1bd2a54e
ras0q <ras0q@users.noreply.github.com>
2023-06-02 18:44:48 +0900

    :bug: fix path

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/ras0q/connect-web-playground/bufgen/go/api/proto/protoconnect"
-	"github.com/ras0q/connect-web-playground/server/internal/handler"
+	"github.com/ras0q/connect-web-playground/internal/handler"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
@@ -14,5 +14,10 @@ func main() {
 	mux := http.NewServeMux()
 	path, handler := protoconnect.NewReadyServiceHandler(rh)
 	mux.Handle(path, handler)
-	http.ListenAndServe(":8080", h2c.NewHandler(mux, &http2.Server{}))
+	if err := http.ListenAndServe(
+		"localhost:8080",
+		h2c.NewHandler(mux, &http2.Server{}),
+	); err != nil {
+		panic(err)
+	}
 }
