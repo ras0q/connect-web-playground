340e528fb0eaf58ee1ba1fe0271cedf6749cb48d
ras0q <ras0q@users.noreply.github.com>
2023-06-02 22:04:21 +0900

    :sparkles: embed frontend

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/ras0q/connect-web-playground/pkg/bufgen/api/proto/protoconnect"
+	playground "github.com/ras0q/connect-web-playground"
 	"github.com/ras0q/connect-web-playground/internal/handler"
+	"github.com/ras0q/connect-web-playground/pkg/bufgen/api/proto/protoconnect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
-	rh := handler.NewReadyHandler()
 	mux := http.NewServeMux()
+	rh := handler.NewReadyHandler()
 	path, handler := protoconnect.NewReadyServiceHandler(rh)
 	mux.Handle(path, handler)
+	mux.Handle("/", http.FileServer(http.FS(playground.PublicFS)))
+
 	if err := http.ListenAndServe(
 		"localhost:8080",
 		h2c.NewHandler(mux, &http2.Server{}),
--- generate.go
+++ generate.go
@@ -1,2 +1,2 @@
 //go:generate go run github.com/bufbuild/buf/cmd/buf@latest generate
-package main
+package playground
